a0b1b8888dac1b03d6ddae18e1c6ddbdab7a5483
Saisruthi0220 <lakshmisruthitedla@gmail.com>
2026-08-11 15:06:02 +0530

    Time: 4 ms (21.13%) | Memory: 69.6 MB (66.70%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -5,15 +5,13 @@ public:
         int left=0,right=0,zCnt=0,maxi=0;
         while(right<n){
             if(nums[right]==0) zCnt++;
-            if(zCnt>k){
+            while(zCnt>k){
                 if(nums[left]==0){
                     zCnt--;
                 }
                 left++;
             }
-            if(zCnt<=k){
-                maxi=max(maxi,right-left+1);
-            }
+            maxi=max(maxi,right-left+1);
             right++;
         }
         return maxi;
